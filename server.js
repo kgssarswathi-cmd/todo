2e4566cfb24f17fd883389cc6f474c6f9d842df4
Saraswathi <kgssarswathi@gmail.com>
2025-10-05 21:50:12 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,31 @@
+//Using express
+const express = require('express');
+
+// create an instance of express
+const app = express();
+app.use(express.json())
+
+//Sample in-memory storage for todo items
+let todos = [];
+
+//Create a new todo item
+app.post('/todos', (req, res) => {
+    const {title, description} = req.body;
+    const newTodo = {
+        id: todos.length + 1,
+        title,
+        description
+    };
+    todos.push(newTodo);
+    console.log(todos);
+    res.status(201).json(newTodo);
+});
+// Example GET route
+app.get('/todos', (req, res) => {
+    res.json(todos);
+});
+//Start the server
+const port = 3000;
+app.listen(port, () => {
+    console.log("Server is listening to port"+ port);
+});
